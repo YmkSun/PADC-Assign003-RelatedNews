25728c1b7e055df8e6aae6919be36d073c2914cf
Ye Myo Kyaw <ymksun90@gmail.com>
2017-11-16 23:44:58 +0630

    Skeleton Structure

---
+++ app/src/main/java/com/androidapp/yemyokyaw/relatednews/MainActivity.java
@@ -0,0 +1,13 @@
+package com.androidapp.yemyokyaw.relatednews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
